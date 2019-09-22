d0412da7f71a81cd01f2a50c550f99a82e8994ad
Stelios Mavrotas <mavrotas.st@gmail.com>
2019-09-22 16:28:19 +0100

    Add files via upload

---
+++ init.m
@@ -0,0 +1,17 @@
+cd=pwd;
+
+
+white=initialRun('W','mixo',[cd,'/startingModels/whiteMIXO_CC_08_09.mat'],'whiteMixoCC');
+red=initialRun('R','mixo',[cd,'/startingModels/redMIXO_CC_08_09.mat'],'redMixoCC');
+blue=initialRun('B','mixo',[cd,'/startingModels/blueMIXO_CC_08_09.mat'],'blueMixoCC');
+
+
+% white=initialRun('W','auto',[cd,'/startingModels/whiteStartingModel_07_15.mat'],'whiteAutoCC');
+% red=initialRun('R','auto',[cd,'/startingModels/redStartingModel_07_15.mat'],'redAutoCC');
+% blue=initialRun('B','auto',[cd,'/startingModels/blueStartingModel_07_15.mat'],'blueAutoCC');
+% white2=initialRun('W','auto',[cd,'/startingModels/whiteOnlyMM_08_05.mat'],'whiteOnlyMM');
+% red2=initialRun('R','auto',[cd,'/startingModels/redOnlyMM_08_05.mat'],'redOnlyMM');
+% blue2=initialRun('B','auto',[cd,'/startingModels/blueOnlyMM_08_05.mat'],'blueOnlyMM');
+% white3=initialRun('W','mixo',[cd,'/startingModels/whiteMIXO_OnlyMM_08_08.mat'],'whiteMixoMM');
+% red3=initialRun('R','mixo',[cd,'/startingModels/redMIXO_OnlyMM_08_09.mat'],'redMixoMM');
+% blue3=initialRun('B','mixo',[cd,'/startingModels/blueMIXO_OnlyMM_08_09.mat'],'blueMixoMM');
